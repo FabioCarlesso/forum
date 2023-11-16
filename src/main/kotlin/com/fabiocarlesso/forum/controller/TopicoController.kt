c03f3bbaaf8e0d883e36cdc6c2a243cce0b1336d
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-11-16 17:57:36 -0300

    feat(forum): Service buscaPorId

--- src/main/kotlin/com/fabiocarlesso/forum/controller/TopicoController.kt
+++ src/main/kotlin/com/fabiocarlesso/forum/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package com.fabiocarlesso.forum.controller
 import com.fabiocarlesso.forum.model.Topico
 import com.fabiocarlesso.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +13,9 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(): List<Topico>{
         return service.listar()
+    }
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscarPorId(id);
     }
 }
--- src/main/kotlin/com/fabiocarlesso/forum/service/TopicoService.kt
+++ src/main/kotlin/com/fabiocarlesso/forum/service/TopicoService.kt
@@ -6,8 +6,8 @@ import com.fabiocarlesso.forum.model.Usuario
 import org.springframework.stereotype.Service
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
+class TopicoService(private var topicos: List<Topico>) {
+    init{
         val topico = Topico(
             id = 1,
             titulo = "Duvida Kotlin",
@@ -23,6 +23,48 @@ class TopicoService {
                 email = "joaosilva@email.com"
             )
         )
-        return listOf(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin",
+            mensagem = "Topico em kotlin",
+            curso = Curso (
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Joao da Silva",
+                email = "joaosilva@email.com"
+            )
+        )
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida Kotlin",
+            mensagem = "Topico em kotlin",
+            curso = Curso (
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Joao da Silva",
+                email = "joaosilva@email.com"
+            )
+        )
+        topicos = listOf(topico, topico2, topico3)
+    }
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream()
+            .filter { t ->
+                t.id == id
+            }
+            .findFirst()
+            .get()
     }
 }
