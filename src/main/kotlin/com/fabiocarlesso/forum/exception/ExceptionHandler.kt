4b952165447dc8182bc4ee3be7279e6ec5c7bcab
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-11-29 11:03:47 -0300

    refactor(forum): Create handler to Method Argument Not Valid Exception

--- src/main/kotlin/com/fabiocarlesso/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/fabiocarlesso/forum/exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.fabiocarlesso.forum.exception
 import com.fabiocarlesso.forum.dto.ErrorView
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -32,6 +33,23 @@ class ExceptionHandler {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exception.message,
+            path = request.servletPath
+        )
+    }
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach(){
+            e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
