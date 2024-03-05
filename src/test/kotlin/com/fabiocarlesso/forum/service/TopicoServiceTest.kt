fdf7017ff8527968914cef57ab6689c4ae0c6f7d
Fabio Carlesso <fabio.carlesso@gmail.com>
2024-03-05 11:44:39 -0300

    test(forum): TopicoServiceTest when it is null

--- src/test/kotlin/com/fabiocarlesso/forum/service/TopicoServiceTest.kt
+++ src/test/kotlin/com/fabiocarlesso/forum/service/TopicoServiceTest.kt
@@ -14,24 +14,31 @@ import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 
 class TopicoServiceTest {
-    val topicos = PageImpl(listOf(TopicoTest.build()))
-    val paginacao: Pageable = mockk()
-    val topicoRepository: TopicoRepository = mockk{
-        every {
-            findByCursoNome(any(), any())
-        } returns topicos
+    private val topicos = PageImpl(listOf(TopicoTest.build()))
+    private val paginacao: Pageable = mockk()
+    private val topicoRepository: TopicoRepository = mockk{
+        every { findByCursoNome(any(), any()) } returns topicos
+        every { findAll(paginacao) } returns topicos
     }
-    val topicoViewMapper: TopicoViewMapper = mockk()
-    val topicoFormMapper: TopicoFormMapper = mockk()
-    val topicoService = TopicoService(
+    private val topicoViewMapper: TopicoViewMapper = mockk(){
+        every { map(any()) } returns TopicoViewTest.build()
+    }
+    private val topicoFormMapper: TopicoFormMapper = mockk()
+    private val topicoService = TopicoService(
         topicoRepository, topicoViewMapper, topicoFormMapper
     )
     @Test
     fun `deve listar a partir do nome do curso`() {
-        every { topicoViewMapper.map(any()) } returns TopicoViewTest.build()
         topicoService.listar("Kotlin avançado", paginacao)
         verify (exactly = 1) { topicoRepository.findByCursoNome(any(), any()) }
         verify (exactly = 1) { topicoViewMapper.map(any()) }
         verify (exactly = 0) { topicoRepository.findAll(paginacao) }
+    }
+    @Test
+    fun `deve listar todos os topicos quando o nome do curso for nulo`() {
+        topicoService.listar(null, paginacao)
+        verify (exactly = 0) { topicoRepository.findByCursoNome(any(), any()) }
+        verify (exactly = 1) { topicoViewMapper.map(any()) }
+        verify (exactly = 1) { topicoRepository.findAll(paginacao) }
     }
 }
