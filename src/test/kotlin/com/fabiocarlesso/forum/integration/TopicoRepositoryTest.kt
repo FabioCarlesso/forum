2fa363014bd44adb8228f6189d9ad4c5cc6f76c0
Fabio Carlesso <fabio.carlesso@gmail.com>
2024-03-11 15:28:25 -0300

    test(forum): Started TopicoRepositoryTest testcontainers configuration

---
+++ src/test/kotlin/com/fabiocarlesso/forum/integration/TopicoRepositoryTest.kt
@@ -0,0 +1,8 @@
+package com.fabiocarlesso.forum.integration
+
+import org.springframework.beans.factory.annotation.Autowired
+
+class TopicoRepositoryTest {
+    @Autowired
+    private lateinit var topicoRepositoryTest: TopicoRepositoryTest
+}
