97246485fbf898e611bc982dc14d69d2a219e12b
Fabio Carlesso <fabio.carlesso@gmail.com>
2024-03-13 15:36:38 -0300

    test(forum): DataJpaTest testcontainers configuration

--- src/test/kotlin/com/fabiocarlesso/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/com/fabiocarlesso/forum/integration/TopicoRepositoryTest.kt
@@ -1,8 +1,34 @@
 package com.fabiocarlesso.forum.integration
 
+import com.fabiocarlesso.forum.repository.TopicoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TopicoRepositoryTest {
     @Autowired
-    private lateinit var topicoRepositoryTest: TopicoRepositoryTest
+    private lateinit var topicoRepository: TopicoRepository
+    companion object {
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing>("mysql:latest").apply {
+            withDatabaseName("testdb")
+            withUsername("test")
+            withPassword("123456")
+        }
+    }
+    @DynamicPropertySource
+    fun properties (registry: DynamicPropertyRegistry){
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+        registry.add("spring.datasource.password", mysqlContainer::getPassword)
+        registry.add("spring.datasource.username", mysqlContainer::getUsername)
+    }
+
 }
